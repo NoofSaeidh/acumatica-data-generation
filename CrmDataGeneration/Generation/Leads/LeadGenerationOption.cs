7e713379e5bc4189185b99346b2f9e6e338535d8
Alexey Kuznetsov <akuznetsov@acumatica.com>
2018-05-10 20:31:33 +0300

    AC-81769: in progress

--- CrmDataGeneration/Generation/Leads/LeadGenerationOption.cs
+++ CrmDataGeneration/Generation/Leads/LeadGenerationOption.cs
@@ -12,36 +12,52 @@ namespace CrmDataGeneration.Generation.Leads
 {
     public class LeadGenerationOption : GenerationOption<Lead>
     {
-        public ProbabilityCollection<string> ConvertToOpportunityByStatus { get; set; }
+        public IDictionary<string, ProbabilityCollection<ConvertLead>> ConvertByStatuses { get; set; }
 
         public override async System.Threading.Tasks.Task RunGeneration(GeneratorClient client, CancellationToken cancellationToken = default)
         {
             CheckSettings();
-            var leads = await client.GenerateAll(this, cancellationToken);
+            var leads = (await client.GenerateAll(this, cancellationToken)).ToList();
 
-            await ConvertLeadsToOpportunities(client, leads, cancellationToken);
-        }
-
-        protected async System.Threading.Tasks.Task ConvertLeadsToOpportunities(GeneratorClient client, IEnumerable<Lead> leads, CancellationToken cancellationToken = default)
-        {
-            if (ConvertToOpportunityByStatus.IsNullOrEmpty())
+            if (ConvertByStatuses.IsNullOrEmpty())
                 return;
 
             // convert depending on Probability defined in ConvertToOpportunityByStatus
 
             var rand = new Randomizer(RandomizerSettings.Seed ?? client.Config.GlobalSeed);
+
+            var convertToOpportunity = new List<Lead>(leads.Count);
+
+            foreach (var lead in leads)
+            {
+                // define should convert (to anything) current lead by status
+                if(!ConvertByStatuses.TryGetValue(lead.Status, out var probabilities))
+                    continue;
+
+                // convert to Opportunities
+                var indexOfStatus = probabilities.IndexOf(ConvertLead.ToOpportunity);
+                if (indexOfStatus >= 0)
+                {
+                    var shouldConvert = rand.Bool((float)probabilities[indexOfStatus].Value);
+                    if (shouldConvert)
+                        convertToOpportunity.Add(lead);
+                }
+
+            }
+
+            if(convertToOpportunity.Any())
+                await ConvertLeadsToOpportunities(client, convertToOpportunity, cancellationToken);
+        }
+
+        protected async System.Threading.Tasks.Task ConvertLeadsToOpportunities(GeneratorClient client, IEnumerable<Lead> leads, CancellationToken cancellationToken = default)
+        {
             var wrappedClient = client.GetApiWrappedClient<Lead>();
             var actionClient = client.GetRawApiClient<InvokeActionClient>();
             foreach (var lead in leads)
             {
-                var indexOfStatus = ConvertToOpportunityByStatus.IndexOf(lead.Status);
-                if (indexOfStatus < 0)
-                    continue;
-
                 var invocation = new ConvertLeadToOpportunity { Entity = lead };
-                var shouldConvert = rand.Bool((float)ConvertToOpportunityByStatus[indexOfStatus].Value);
-                if(shouldConvert)
-                    await wrappedClient.WrapAction(actionClient.ConvertLeadToOpportunityAsync(invocation, cancellationToken));
+
+                await wrappedClient.WrapAction(actionClient.ConvertLeadToOpportunityAsync(invocation, cancellationToken));
             }
         }
     }
