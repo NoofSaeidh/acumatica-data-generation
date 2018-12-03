f114d5725c9c948a264bfb933bf345ffd1ca467c
Alexey Kuznetsov <noofsaeidh@outlook.com>
2018-12-03 20:29:27 +0300

    split JsonFileCacheHelper and move base functionality to BaseFileCacheHelper

---
+++ DataGeneration/Common/BaseFileCacheHelper.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.IO;
+
+namespace DataGeneration.Common
+{
+    public abstract class BaseFileCacheHelper
+    {
+        public const string DefaultCacheFolder = "caches";
+        public const string DefaultFileExtension = "cache";
+        public virtual string CacheFolder { get; } = DefaultCacheFolder;
+        public virtual string FileExtension { get; } = DefaultFileExtension;
+
+        protected BaseFileCacheHelper()
+        {
+            if (!Directory.Exists(CacheFolder))
+            {
+                Directory.CreateDirectory(CacheFolder);
+            }
+        }
+
+        public void DeleteCache(string cacheName)
+        {
+            if (cacheName == null)
+                throw new ArgumentNullException(nameof(cacheName));
+
+            DeleteFileCache(GetCachePath(cacheName));
+        }
+
+        public virtual string GetCachePath(string cacheName)
+        {
+            return CacheFolder + '\\' + cacheName + '.' + FileExtension;
+        }
+
+        public bool IsCacheExist(string cacheName)
+        {
+            if (cacheName == null)
+                throw new ArgumentNullException(nameof(cacheName));
+
+            return IsFileCacheExist(GetCachePath(cacheName));
+        }
+
+        public T ReadFromCache<T>(string cacheName)
+        {
+            if (cacheName == null)
+                throw new ArgumentNullException(nameof(cacheName));
+
+            return ReadFromFileCache<T>(GetCachePath(cacheName));
+        }
+
+        public T ReadFromCacheAndDelete<T>(string cacheName, bool deleteOnFailure = false)
+        {
+            if (cacheName == null)
+                throw new ArgumentNullException(nameof(cacheName));
+
+            var path = GetCachePath(cacheName);
+
+            try
+            {
+                var result = ReadFromFileCache<T>(path);
+                TryDeleteFileCache(path);
+                return result;
+            }
+            catch
+            {
+                if (deleteOnFailure)
+                    TryDeleteFileCache(path);
+                throw;
+            }
+        }
+
+        public T ReadFromCacheOrSave<T>(string cacheName, Func<T> factory)
+        {
+            if (cacheName == null)
+                throw new ArgumentNullException(nameof(cacheName));
+            if (factory == null)
+                throw new ArgumentNullException(nameof(factory));
+
+            var path = GetCachePath(cacheName);
+
+            if (TryReadFromFileCache<T>(path, out var cacheResult))
+                return cacheResult;
+
+            var result = factory();
+            SaveFileCache(path, result);
+            return result;
+        }
+
+        public void SaveCache(string cacheName, object value)
+        {
+            if (cacheName == null)
+                throw new ArgumentNullException(nameof(cacheName));
+            if (value == null)
+                throw new ArgumentNullException(nameof(value));
+
+            SaveFileCache(GetCachePath(cacheName), value);
+        }
+
+        public bool TryDeleteCache(string cacheName)
+        {
+            if (cacheName == null)
+                throw new ArgumentNullException(nameof(cacheName));
+
+            return TryDeleteFileCache(GetCachePath(cacheName));
+        }
+
+        public bool TryReadFromCache<T>(string cacheName, out T result)
+        {
+            if (cacheName == null)
+                throw new ArgumentNullException(nameof(cacheName));
+            return TryReadFromFileCache(GetCachePath(cacheName), out result);
+        }
+
+        public bool TryReadFromCacheAndDelete<T>(string cacheName, out T result, bool deleteOnFailure = false)
+        {
+            if (cacheName == null)
+                throw new ArgumentNullException(nameof(cacheName));
+            var path = GetCachePath(cacheName);
+            var success = TryReadFromFileCache<T>(path, out result);
+            if (success || deleteOnFailure)
+            {
+                TryDeleteFileCache(path);
+            }
+            return success;
+        }
+
+        #region Protected Virtual
+
+        protected virtual void DeleteFileCache(string path)
+        {
+            if (!IsFileCacheExist(path))
+                throw new InvalidOperationException("Cache doesn't exist.");
+
+            File.Delete(path);
+        }
+
+        protected virtual bool IsFileCacheExist(string path)
+        {
+            return File.Exists(path);
+        }
+
+        protected virtual T ReadFromFileCache<T>(string path)
+        {
+            return ParseCache<T>(File.ReadAllText(path));
+        }
+
+        protected virtual void SaveFileCache(string path, object value)
+        {
+            File.WriteAllText(path, SerializeCache(value));
+        }
+
+        protected abstract T ParseCache<T>(string cacheText);
+
+        protected abstract string SerializeCache(object value);
+
+        protected virtual bool TryDeleteFileCache(string path)
+        {
+            if (!IsFileCacheExist(path))
+                return false;
+
+            try
+            {
+                DeleteFileCache(path);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        protected virtual bool TryReadFromFileCache<T>(string path, out T result)
+        {
+            if (!IsFileCacheExist(path))
+            {
+                result = default;
+                return false;
+            }
+            try
+            {
+                result = ReadFromFileCache<T>(path);
+                return true;
+            }
+            catch
+            {
+                result = default;
+                return false;
+            }
+        }
+
+        #endregion
+    }
+}
--- DataGeneration/Common/JsonFileCacheHelper.cs
+++ DataGeneration/Common/JsonFileCacheHelper.cs
@@ -1,26 +1,19 @@
 ﻿using Newtonsoft.Json;
 using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace DataGeneration.Common
 {
-    public class JsonFileCacheHelper
+    public class JsonFileCacheHelper : BaseFileCacheHelper
     {
-        public const string CacheFolder = "caches\\";
-        public const string FileExtension = ".cache.json";
+        public new const string DefaultFileExtension = ".cache.json";
+        public override string FileExtension => DefaultFileExtension;
 
         private readonly JsonSerializerSettings _jsonSettings;
 
         public JsonFileCacheHelper()
         {
-            if(!Directory.Exists(CacheFolder))
-            {
-                Directory.CreateDirectory(CacheFolder);
-            }
             _jsonSettings = new JsonSerializerSettings
             {
                 NullValueHandling = NullValueHandling.Ignore,
@@ -30,81 +23,14 @@ namespace DataGeneration.Common
 
         public static JsonFileCacheHelper Instance { get; } = new JsonFileCacheHelper();
 
-        public string GetCacheFileName(string cacheName)
+        protected override T ParseCache<T>(string cacheText)
         {
-            return CacheFolder + cacheName + FileExtension;
+            return JsonConvert.DeserializeObject<T>(cacheText, _jsonSettings);
         }
 
-        public T ReadFromCache<T>(string cacheName)
+        protected override string SerializeCache(object value)
         {
-            if (cacheName == null)
-                throw new ArgumentNullException(nameof(cacheName));
-
-            return JsonConvert.DeserializeObject<T>(File.ReadAllText(GetCacheFileName(cacheName)), _jsonSettings);
-        }
-
-        public bool TryReadFromCache<T>(string cacheName, out T result)
-        {
-            if (cacheName == null)
-                throw new ArgumentNullException(nameof(cacheName));
-
-            if(!CacheExist(cacheName))
-            {
-                result = default;
-                return false;
-            }
-            try
-            {
-                result = ReadFromCache<T>(cacheName);
-                return true;
-            }
-            catch
-            {
-                result = default;
-                return false;
-            }
-        }
-
-        public bool CacheExist(string cacheName)
-        {
-            return File.Exists(GetCacheFileName(cacheName));
-        }
-
-        public void SaveCache(string cacheName, object value)
-        {
-            File.WriteAllText(GetCacheFileName(cacheName), JsonConvert.SerializeObject(value, _jsonSettings));
-        }
-
-        public T ReadFromCacheOrSave<T>(string cacheName, Func<T> factory)
-        {
-            if (cacheName == null)
-                throw new ArgumentNullException(nameof(cacheName));
-            if (factory == null)
-                throw new ArgumentNullException(nameof(factory));
-
-            if (TryReadFromCache<T>(cacheName, out var cacheResult))
-                return cacheResult;
-
-            var result = factory();
-            SaveCache(cacheName, result);
-            return result;
-        }
-
-        public async Task<T> ReadFromCacheOrSave<T>(string cacheName, Func<Task<T>> factory)
-        {
-            if (cacheName == null)
-                throw new ArgumentNullException(nameof(cacheName));
-            if (factory == null)
-                throw new ArgumentNullException(nameof(factory));
-
-            await Task.Yield();
-
-            if (TryReadFromCache<T>(cacheName, out var cacheResult))
-                return cacheResult;
-
-            var result = await factory();
-            SaveCache(cacheName, result);
-            return result;
+            return JsonConvert.SerializeObject(value, _jsonSettings);
         }
     }
 }
